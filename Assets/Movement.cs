e3801db5f2351e43b578bae16c3dbee7c46f081a
Jesús González <ito123456789@gmail.com>
2018-07-16 07:27:20 -0400

    Working on character controller.

---
+++ Assets/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+
+    public float gravity;
+
+    Vector2 velocity;
+
+    Collider2D collider;
+
+    void Start()
+    {
+        velocity = Vector2.zero;
+
+        collider = GetComponent<Collider2D>();
+    }
+    
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        Vector2 newPosition = transform.position;
+        
+        // Apply gravity
+        velocity.y -= gravity * Time.deltaTime;
+        
+        // Clamp velocity
+        Mathf.Clamp(velocity.x, -speed, speed);
+        Mathf.Clamp(velocity.y, -speed, speed);
+        
+        // Set new position
+        newPosition += velocity;
+        transform.position = newPosition;
+    }
+
+    bool CheckCollision(Vector2 position)
+    {
+        if (collider.bounds.Intersects())
+        
+        return true;
+    }
+}
